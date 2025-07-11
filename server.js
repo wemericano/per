8b0c18797097ca02a2c3ebaea461aa623574203b
위동균 <dgwi@mysoftwiz.com>
2025-07-11 12:21:04 +0900

    최종 서버 수정

--- server.js
+++ server.js
@@ -14,3 +14,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`서버 실행 중: http://localhost:${PORT}`);
 });
+
+
+/*
+git add .
+git commit -m "최종 서버 수정"
+git push origin main
+*/
